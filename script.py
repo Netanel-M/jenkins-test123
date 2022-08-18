b9f65ddb687856cb88696612c8f547b609efa844
Your Name <you@example.com>
2022-08-18 17:55:45 +0300

    fix

--- script.py
+++ script.py
@@ -4,4 +4,4 @@ import sys
 argument = sys.argv[1]
 with open("artifact.txt", "w") as file_handler:
   for i in range(int(argument)):
-    file_handler.writelines([i])
+    file_handler.writelines([str(i)])
